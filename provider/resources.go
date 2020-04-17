b0f809ee7ddea116dc41d398d28ebfe1043077d8
stack72 <public@paulstack.co.uk>
2020-04-17 21:07:56 +0100

    Upgrade to v2.0.0 of Pulumi

--- provider/resources.go
+++ provider/resources.go
@@ -251,7 +251,7 @@ func Provider() tfbridge.ProviderInfo {
 		JavaScript: &tfbridge.JavaScriptInfo{
 			AsyncDataSources: true,
 			Dependencies: map[string]string{
-				"@pulumi/pulumi":    "^2.0.0-beta.2",
+				"@pulumi/pulumi":    "^2.0.0",
 				"builtin-modules":   "3.0.0",
 				"read-package-tree": "^5.2.1",
 				"resolve":           "^1.7.1",
@@ -275,12 +275,12 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		Python: &tfbridge.PythonInfo{
 			Requires: map[string]string{
-				"pulumi": ">=2.0.0b2,<3.0.0",
+				"pulumi": ">=2.0.0,<3.0.0",
 			},
 		},
 		CSharp: &tfbridge.CSharpInfo{
 			PackageReferences: map[string]string{
-				"Pulumi":                       "2.0.0-beta.2",
+				"Pulumi":                       "2.*",
 				"System.Collections.Immutable": "1.6.0",
 			},
 			Namespaces: map[string]string{
--- scripts/go.mod
+++ scripts/go.mod
@@ -2,4 +2,4 @@ module github.com/pulumi/pulumi-digitalocean/scripts/v2
 
 go 1.13
 
-require github.com/pulumi/pulumi/sdk/v2 v2.0.0-beta.3
+require github.com/pulumi/pulumi/sdk/v2 v2.0.0
