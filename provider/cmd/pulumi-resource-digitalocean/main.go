ffcca069e4eddb57498ab37f5aa8a0badc7964ac
Ian Wahbe <ian@wahbe.com>
2024-01-16 11:48:51 -0800

    lint_provider.fix

--- provider/cmd/pulumi-resource-digitalocean/main.go
+++ provider/cmd/pulumi-resource-digitalocean/main.go
@@ -19,9 +19,10 @@ package main
 import (
 	_ "embed"
 
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+
 	digitalocean "github.com/pulumi/pulumi-digitalocean/provider/v4"
 	"github.com/pulumi/pulumi-digitalocean/provider/v4/pkg/version"
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 )
 
 //go:embed schema-embed.json
